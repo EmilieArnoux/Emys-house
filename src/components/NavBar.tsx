1688bab9a6c9d7bc47225db61719472252a5c827
Emilie Arnoux <arnoux.emilie@live.fr>
2025-06-02 00:46:59 +0200

    modif css

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,7 +3,10 @@ import React from "react";
 const NavBar = () => {
       return (
     <nav className="navbar">
-      <div className="emyhouse">Emy's House</div>
+      <div className="burger-image">
+      </div>
+      <div className="emyhouse">
+      <img src="src/assets/burger-au-fromage.png" alt="burger" className="avatar"/>Emy's House</div>
       <ul className="presentation">
         <li><a href="#" className="accueil">Accueil</a></li>
         <li><a href="#" className="propos">À propos</a></li>
